f4d00a6ff97d45e1a348963801e0ce6a0dd29932
gabycode <86427446+gabycode@users.noreply.github.com>
2024-10-17 14:22:46 -0400

    add: email js function

--- index.js
+++ index.js
@@ -5,3 +5,41 @@ document.addEventListener("DOMContentLoaded", function () {
       document.getElementById("navbar").innerHTML = data;
     });
 });
+
+(function () {
+  emailjs.init("bwwSwF-PGCgP7YbpF");
+})();
+
+emailjs.init("bwwSwF-PGCgP7YbpF");
+
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    // Recopila los datos del formulario
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+
+    // Define los parámetros que se enviarán a EmailJS
+    const templateParams = {
+      name: name,
+      email: email,
+      message: message,
+    };
+
+    // Envía el correo usando EmailJS
+    emailjs.send("TU_SERVICE_ID", "TU_TEMPLATE_ID", templateParams).then(
+      function (response) {
+        console.log(
+          "Correo enviado con éxito!",
+          response.status,
+          response.text
+        );
+      },
+      function (error) {
+        console.log("Error al enviar el correo", error);
+      }
+    );
+  });
