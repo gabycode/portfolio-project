cb459bc1ae7ad57ffcdb3ce35e203069ef3bbd59
gabycode <86427446+gabycode@users.noreply.github.com>
2024-11-05 13:07:59 -0400

    add: suggestion box logic

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Side nav bar appear in all pages
 document.addEventListener("DOMContentLoaded", function () {
   fetch("navbar.html")
     .then((response) => response.text())
@@ -6,6 +7,48 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+// Suggestions box form
+
+const suggestions = [];
+document.getElementById("suggestions-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const nameSuggestion = document.getElementById("nombre-sugerencias").value;
+  const messageSuggestion = document.getElementById(
+    "mensaje-sugerencias"
+  ).value;
+
+  const newSuggestion = {
+    name: nameSuggestion,
+    message: messageSuggestion,
+  };
+
+  suggestions.push(newSuggestion);
+
+  displaySuggestion();
+  document.getElementById("suggestions-form").reset();
+  console.log(suggestions);
+});
+
+const displaySuggestion = () => {
+  const suggestionsBox = document.getElementById("suggestions-box");
+
+  suggestionsBox.innerHTML = "";
+
+  suggestions.forEach((suggestion) => {
+    const suggestionDiv = document.createElement("div");
+    suggestionDiv.classList.add("suggestion-entry");
+
+    suggestionDiv.innerHTML = `
+      <strong>${suggestion.name}</strong> <br>
+      <p>${suggestion.message}</p>
+    `;
+
+    suggestionsBox.appendChild(suggestionDiv);
+  });
+};
+
+// Contact form - EmailJS integration
 document
   .getElementById("contact-form")
   .addEventListener("submit", function (event) {
