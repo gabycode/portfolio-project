23aa8f0c6ece3dc3fafc6de17b894b5a04c5ac6b
gabycode <86427446+gabycode@users.noreply.github.com>
2024-11-11 14:19:19 -0400

    add: email js script

---
+++ email.js
@@ -0,0 +1,36 @@
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const name = document.getElementById("nombre").value;
+    const telefono = document.getElementById("telefono").value;
+    const correo = document.getElementById("correo").value;
+    const mensaje = document.getElementById("mensaje").value;
+
+    const templateParams = {
+      from_name: name,
+      to_name: "Gabriella",
+      from_email: correo,
+      to_email: "gabriellarosario72@gmail.com",
+      from_phone: telefono,
+      message: mensaje,
+    };
+
+    emailjs.send("service_mz43gdb", "template_d4iml9n", templateParams).then(
+      function (response) {
+        console.log(
+          "Correo enviado con éxito!",
+          response.status,
+          response.text,
+          console.log(templateParams)
+        );
+        alert("¡Mensaje enviado con éxito!");
+        event.target.reset();
+      },
+      function (error) {
+        console.log("Error al enviar el correo", error);
+        alert("Error al enviar correo, intenta de nuevo.");
+      }
+    );
+  });
